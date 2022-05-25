806299b8adfc52aab0812906c4be9676b9cee54a
Yalton <73573710+Yalton@users.noreply.github.com>
2022-05-25 11:38:26 -0700

    webCrawler added

--- datahoarding/websiteMirrorer.py
+++ datahoarding/websiteMirrorer.py
@@ -3,5 +3,12 @@ import os
 
 mypath = "/mnt/nas/shared/DataHoard/Websites"
 filenames = os.listdir(mypath)
-# filenames = next(walk(mypath), (None, None, []))[2]  # [] if no file
+
 print (filenames)
+
+for url in filenames: 
+    url = 'https://' + url
+
+for url in filenames: 
+    os.system(f"""wget --recursive --html-extension --page-requisites --convert-links {url} -P {mypath}""")
+    # wget.download(url, out=output_directory)
