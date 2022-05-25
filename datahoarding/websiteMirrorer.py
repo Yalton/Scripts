179e194a62b602649d881acba9cfb0c0bc34dfb3
Yalton <73573710+Yalton@users.noreply.github.com>
2022-05-25 11:18:42 -0700

    sadi

--- datahoarding/websiteMirrorer.py
+++ datahoarding/websiteMirrorer.py
@@ -1,6 +1,7 @@
-from os import walk
+import os
 
 
 mypath = "/mnt/nas/shared/DataHoard/Websites"
-filenames = next(walk(mypath), (None, None, []))[2]  # [] if no file
+filenames = os.listdir(mypath)
+# filenames = next(walk(mypath), (None, None, []))[2]  # [] if no file
 print (filenames)
