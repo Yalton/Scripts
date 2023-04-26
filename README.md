cdbdf06d81eb1236c994726cb91b49db5a928ba6
yalt <yalt@databox>
2023-04-25 18:59:17 -0700

    yes

--- README.md
+++ README.md
@@ -1,5 +1,38 @@
-# General Purpose Scripts
+<!-- PROJECT SHIELDS -->
+Contributors
+Forks
+Stargazers
+Issues
+MIT License
 
-![writing_bot](writing_bot.png)
+<!-- PROJECT LOGO -->
+<br />
+<div align="center">
+  <a href="https://github.com/github_username/general-purpose-scripts">
+    <img src="writing_bot.png" alt="Logo" width="80" height="80">
+  </a>
+<h3 align="center">General Purpose Scripts</h3>
+  <p align="center">
+    A collection of useful scripts for various purposes made with Bash and Python.
+    <br />
+    <a href="https://github.com/github_username/general-purpose-scripts"><strong>Explore the docs »</strong></a>
+    <br />
+    <br />
+    <a href="https://github.com/github_username/general-purpose-scripts">View Demo</a>
+    ·
+    <a href="https://github.com/github_username/general-purpose-scripts/issues">Report Bug</a>
+    ·
+    <a href="https://github.com/github_username/general-purpose-scripts/issues">Request Feature</a>
+  </p>
+</div>
+<!-- CONTACT -->
+Contact
+Your Name - @twitter_handle - email@email_client.com
 
-This repository contains general purpose scripts that can be used for a variety of purposes.
+Project Link: https://github.com/github_username/general-purpose-scripts
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+<!-- ACKNOWLEDGMENTS -->
+Acknowledgments
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+<!-- MARKDOWN LINKS & IMAGES -->
--- datahoarding/websiteMirrorer.py
+++ datahoarding/websiteMirrorer.py
@@ -1,27 +1,39 @@
 import os
 import subprocess
 import argparse
+import time
+from concurrent.futures import ThreadPoolExecutor
 
-def getArgs(): 
-
+def getArgs():
     parser = argparse.ArgumentParser()
     parser.add_argument('--path', '-p', type=str, required=True)
+    parser.add_argument('--delay', '-d', type=int, default=0, help="Delay between downloads in seconds")
+    parser.add_argument('--workers', '-w', type=int, default=4, help="Number of parallel downloads")
     return parser.parse_args()
+
+def download_website(url, original_name, mypath, delay):
+    target_path = os.path.join(mypath, original_name)
+    wget_command = f"""wget --mirror --convert-links --adjust-extension --page-requisites --no-parent -e robots=off --backup-converted=off --directory-prefix {mypath} {url}"""
+    print(wget_command)
+    time.sleep(delay)
+    try:
+        subprocess.check_output(wget_command, shell=True, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as e:
+        print(f"Error downloading {url}: {e.output.decode('utf-8')}")
 
 def main():
     args = getArgs()
     mypath = args.path
+    delay = args.delay
+    workers = args.workers
     filenames = os.listdir(mypath)
 
-    #print (filenames)
+    # Remove "www." from the beginning of each URL
+    cleaned_urls = ['https://' + url.replace('www.', '') for url in filenames]
 
-    for url in filenames: 
-        url = 'https://' + url
+    with ThreadPoolExecutor(max_workers=workers) as executor:
+        for url, original_name in zip(cleaned_urls, filenames):
+            executor.submit(download_website, url, original_name, mypath, delay)
 
-    for url in filenames: 
-        print(f"""wget -q --recursive --html-extension --page-requisites --convert-links {url} -P {mypath}""")
-        #os.system(f"""wget -q --recursive --html-extension --page-requisites --convert-links {url} -P {mypath}""")
-        subprocess.Popen(f"""wget -q --recursive --html-extension --page-requisites --convert-links {url} -P {mypath}""", shell=True)
-
-if __name__ == "__main__": 
+if __name__ == "__main__":
     main()
