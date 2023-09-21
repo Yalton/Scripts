f22372bbef4267f7328e933ae7a31939516eda77
DaltonBailey <drbailey117@gmail.com>
2023-09-21 18:56:33 -0500

    Script modded

--- setup_zsh/setup_zsh.sh
+++ setup_zsh/setup_zsh.sh
@@ -2,13 +2,14 @@
 
 # Update and install zsh
 sudo apt update
-sudo apt install zsh
+sudo apt install zsh -y
 
 # Change the default shell to zsh
 chsh -s $(which zsh)
 
 # Install Oh My Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "Y" | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Backup .bashrc and .zshrc
 cp ~/.bashrc ~/.bashrc.backup
@@ -22,4 +23,3 @@ nano ~/.zshrc
 
 # Source .zshrc
 source ~/.zshrc
-
